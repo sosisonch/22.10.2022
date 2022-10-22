9278aa190f963202e2ffa6cea9168bb9cb28abae
sosisonch <114304489+sosisonch@users.noreply.github.com>
2022-10-22 19:25:34 +0300

    Add files via upload

---
+++ 2022.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	//	Series1◦.Даны десять вещественных чисел.Найти их сумму.
+/*	double x, sum = 0;
+	int i = 1;
+
+	while (i <= 10) {
+		cin >> x;
+		sum += x;
+		i++;
+	}
+	cout << sum; */
+	//Series2.Даны десять вещественных чисел.Найти их произведение.
+	/*double x, pr = 1.0;
+	for (int i = 1; i <= 10; i++)
+	{
+		cin >> x;
+		pr = pr * x;
+	}
+	cout << pr << endl;*/
+//   Series3.Даны десять вещественных чисел.Найти их среднее арифметическое.
+	/*double x, sum = 0;
+	int i = 1;
+	double aref = 0;
+	while (i <= 10) {
+		cin >> x;
+		sum += x;
+		aref = sum / 10;
+		i++;
+	}
+	cout << aref; */
+	//Series4.Дано целое число N и набор из N вещественных чисел.Вывести
+		//сумму и произведение чисел из данного набора.
+	/*int N;
+	double x, sum=0,pr=1;
+	cin >> N ;
+	for (int i = 1; i <= N; i++) {
+	cin >> x;
+	sum += x;
+	pr =pr* x;
+	}
+	cout << sum << " " << pr << endl;
+	*/
+	 
+
+}
